6c1779e23ad3a7b66a204e53543a6acee2d37c23
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-04-06 16:01:12 +0900

    프로젝트 데이터 생성

---
+++ src/db/Project_db.js
@@ -0,0 +1,23 @@
+export const projects = [
+  {
+    id: 1,
+    title: "프로젝트 이름 1",
+    imgFile: {},
+    description: "프로젝트 설명 1",
+    tectStack: "기술스택",
+  },
+  {
+    id: 2,
+    title: "프로젝트 이름 2",
+    imgFile: {},
+    description: "프로젝트 설명 2",
+    tectStack: "기술스택",
+  },
+  {
+    id: 3,
+    title: "프로젝트 이름 3",
+    imgFile: {},
+    description: "프로젝트 설명 3",
+    tectStack: "기술스택",
+  },
+];
